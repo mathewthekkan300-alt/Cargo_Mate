3ff86fa699ae0a21440765765a0b20ec24bc2c6f
mathewthekkan300-alt <mathewthekkan300@gmail.com>
2026-06-21 13:18:45 +0200

    Add files via upload

---
+++ www/sw.js
@@ -0,0 +1,12 @@
+const CACHE = "cargomate-v10";
+const ASSETS = ["index.html", "manifest.webmanifest", "ship.svg", "icon.svg", "icon-192.png", "icon-512.png"];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match("index.html"))));
+});
